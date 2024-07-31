2a8c9cd8571703f9e3a94c2a24e26c745ab62412
Faisal <runescape1724@gmail.com>
2024-07-30 23:11:08 -0400

    fixed api component vercel build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Login } from "@/pages/auth/signin";
+import { Login } from "@/pages/signin";
 import { RiddlePage } from "@/components/ui/riddle";
 import { Task } from "@/components/ui/task";
 import { useRouter } from "next/router";
